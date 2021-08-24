3c8a8f3b386540849fcffdae8297ba9ce8bd2c5c
Shaurya Pethe <rajesh.pethe+shaurya@gmail.com>
2021-08-24 18:28:12 +0530

    practicing object oriented programming part 1

---
+++ oop_pt1.py
@@ -0,0 +1,25 @@
+class Animal():
+
+    def __init__(self, fur):
+        self.fur = fur
+
+        print('animal created')
+
+    def report(self):
+        print('animal')
+
+    def eat(self):
+        print('eating')
+
+class Dog(Animal):
+
+    def __init__(self, fur):
+        Animal.__init__(self, fur)
+        print('dog created')
+
+    def report(self):
+        print('I am a dog')
+
+d = Dog('Fuzzy')
+print(d.fur)
+
